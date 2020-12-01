56e8e378e8bfa57fbe2c0c489ab68ce78fde034b
gulistanay <53774762+gulistanay@users.noreply.github.com>
2020-12-01 13:18:12 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Soru_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(69, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 148);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(288, 48);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(125, 148);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.Click += new System.EventHandler(this.listBox2_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(69, 225);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(119, 148);
+            this.listBox3.TabIndex = 2;
+            this.listBox3.Click += new System.EventHandler(this.listBox3_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(288, 225);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(117, 148);
+            this.listBox4.TabIndex = 3;
+            this.listBox4.Click += new System.EventHandler(this.listBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+
+    }
+
+  
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soru_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = -525; i <= 525; i++)           //İlgili ListBox'a tıklanıldığında değerler ekrana geliyor.
+            {
+                if (i % 4 == 0 && i % 3 == 0)       //Aynı anda 3'e ve 4'e bölünüyorsa
+                {
+
+                    string bolunen;
+                    bolunen = Convert.ToString(i);
+                    listBox1.Items.Add(bolunen);       
+                }
+
+            }
+        }
+
+        private void listBox2_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = -525; i <= 525; i++)
+            {
+                if (i % 3 == 0 && i % 4 != 0)       //3'e bölünüp 4'e bölünemiyorsa
+                {
+                    string bolunen;
+                    bolunen = Convert.ToString(i);
+                    listBox2.Items.Add(bolunen);
+                }
+
+            }
+        }
+
+        private void listBox3_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = -525; i <= 525; i++)
+            {
+                if (i % 3 != 0 && i % 4 == 0)           //4'e bölünüp 3'e bölünemiyorsa
+                {
+                    string bolunen;
+                    bolunen = Convert.ToString(i);
+                    listBox3.Items.Add(bolunen);
+                }
+
+            }
+        }
+
+        private void listBox4_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = -525; i <= 525; i++)
+            {
+                if (i % 3 != 0 && i % 4 != 0)           //Hem 3'e hem de 4'e bölünemiyorsa
+                {
+                    string bolunen;
+                    bolunen = Convert.ToString(i);
+                    listBox4.Items.Add(bolunen);
+                }
+
+            }
+        }
+    }
+}
